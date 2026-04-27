9b0c6d395389f3e52a0f2f5bf5a13852cab39d3f
mohammadali19108-cell <mohammadali19108@gmail.com>
2026-04-27 17:57:18 +0330

    3

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App () { 
+
+
+  return (
+    <div className="div1">
+    <div className="div2">
+      <p className="matn1">React todo</p>
+      <p className="matn2">Application For TodoList</p>
+    </div>
+    </div>
+  )
+
+
+}
