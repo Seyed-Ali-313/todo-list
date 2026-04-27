6d114e9154aab2fb6711a60d3c170e4c2ba4d390
mohammadali19108-cell <mohammadali19108@gmail.com>
2026-04-27 18:06:18 +0330

    ث

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App () {
   return (
     <div className="div1">
     <div className="div2">
-      <p className="matn1">React todo</p>
+      <p className="matn1">Reatodo</p>
       <p className="matn2">Application For TodoList</p>
     </div>
     </div>
